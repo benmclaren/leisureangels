91a0b11ff00fcac3477b5c570a524a1eb05257fb
Ben McLaren <benmclaren@live.co.uk>
2024-07-10 15:48:21 +0100

    added tailwind

--- assets/scss/postcss.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-    plugins: [
-        require('postcss-import')({
-            path: ["assets/scss"]
-        }),
-        require('tailwindcss')('assets/scss/tailwind.config.js'),
-        require('autoprefixer')
-    ]
-};
